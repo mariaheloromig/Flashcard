8e6adcc3d70c9f45a4d1734804b550c7ee014003
mariaheloromig <maria.romig@escola.pr.gov.br>
2025-06-27 01:22:25 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,63 @@
+criaCartao(
+    'Astronomia',
+    'Qual é o maior planeta do sistema solar?',
+    'Jupiter'
+)
+
+criaCartao(
+    'Biologia',
+    'Para que serve o DNA?',
+    'Armazena as informações genéticas do organismo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'Paris'
+)
+
+criaCartao(
+    'História',
+    'Qual a data de início da Segunda Guerra Mundial?',
+    '1 de setembro de 1939'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo?',
+    'Vaticano'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantos segundos tem uma hora?',
+    '3600 segundos '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior oceano do mundo?',
+    'Oceano Pacífico'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento químico representado pelo símbolo H?',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos corações tem um polvo?',
+    'Três'
+)
+
+
+
+
