ef71dc1d342c91f8dbba578c49f8e53b996625c0
mariaheloromig <maria.romig@escola.pr.gov.br>
2025-06-28 17:32:38 -0300

    .

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ function criaCartao (categoria, pergunta, resposta) {
 
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-    <h3>Astronomia</h3>
+    <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-        <p>Qual é o maior planeta do sistema solar?</p>
+        <p>${pergunta}</p>
+    </div>
     <div class="cartao__conteudo__resposta">
-         <p>Jupiter</p>
-     </div>
-     </div>
+         <p>${resposta}</p>
+    </div>
     </div>
     `
     container.appendChild(cartao)
