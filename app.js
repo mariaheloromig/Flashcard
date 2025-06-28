6f078c7d567840b6fed0111bb4a74049204e0d1b
mariaheloromig <maria.romig@escola.pr.gov.br>
2025-06-28 12:03:43 -0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function criaCartao (categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>Astronomia</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>Qual é o maior planeta do sistema solar?</p>
+    <div class="cartao__conteudo__resposta">
+         <p>Jupiter</p>
+     </div>
+     </div>
+    </div>
+    `
+    container.appendChild(cartao)
 }
